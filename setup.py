c9c48caa9c2390068eb43b11a54cf0a995c6cb85
Brian Park <brian@xparks.net>
2018-01-01 21:58:40 -0800

    Change package name to 'bigquery_schema_generator' for compatibility with initial 'setup.py' for PyPI.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bigquery-schema-generator',
+      version='0.1',
+      description='BigQuery schema generator',
+      url='https://github.com/bxparks/bigquery-schema-generator',
+      author='Brian T. Park',
+      author_email='brian@xparks.net',
+      license='Apache 2.0',
+      packages=['bigquery_schema_generator'],
+      python_requires='~=3.5')
