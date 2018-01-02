19dbbef07f3b52ee53d8d236c1fb0e4b8d78a477
Brian Park <brian@xparks.net>
2018-01-01 21:41:34 -0800

    Use /usr/bin/env to support virtualenv and venv.

--- generator/data_reader.py
+++ generator/data_reader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 #
 # Copyright 2017 Brian T. Park
 #
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Parses the 'testdata.txt' date file used by the 'generate_schema_test.py'
+Parses the 'testdata.txt' date file used by the 'test_generate_schema.py'
 program.
 
 Usage:
